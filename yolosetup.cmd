39b79b3955a530ce760c5a9ffc1a2e191c8b940e
Muhammad Hassan <mohammad555.hassan@gmail.com>
2025-01-06 14:14:00 +0500

    Create yolosetup.cmd

---
+++ yolosetup.cmd
@@ -0,0 +1,21 @@
+@echo off
+:: Create a Conda environment for YOLO
+echo Creating a Conda environment for YOLO...
+conda create -n yolovenv python=3.8 -y
+
+:: Activate the environment
+echo Activating the Conda environment...
+call conda activate yolovenv
+
+:: Install necessary Python packages
+echo Installing YOLO dependencies...
+pip install ultralytics
+
+:: Install LabelImg
+echo Installing LabelImg...
+pip install labelimg
+
+:: Confirm setup
+echo YOLO environment setup is complete. You can now use the 'yolovenv' environment.
+echo To activate it manually, use: conda activate yolovenv
+pause
